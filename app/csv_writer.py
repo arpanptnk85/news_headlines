be35aa31405cf0724670af89e913569182d3c615
Arpan Patnaik <arpan.patnaik@hyperkurve.com>
2024-09-05 23:13:03 +0530

    feat: csv file o/p as headlines

--- app/csv_writer.py
+++ app/csv_writer.py
@@ -3,7 +3,7 @@ import csv
 class CSVWriter():
     def write(filepath: str, data: list[dict[str, str]], headers: list[str]) -> bytes | None:
         with open(filepath, 'w', newline='') as csvfile:
-            writer = csv.DictWriter(csvfile, fieldnames=headers, delimiter='\t')
+            writer = csv.DictWriter(csvfile, fieldnames=headers, delimiter=',')
             writer.writeheader()
             for row in data:
                 writer.writerow(row)
